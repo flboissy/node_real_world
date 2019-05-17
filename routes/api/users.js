5a9b4ed72bab97457da5daa1076b28ccf8547f02
flboissy <boissy.florent@gmail.com>
2019-05-17 11:17:10 +0200

    get put ok

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,72 +3,88 @@ var router = require('express').Router();
 var passport = require('passport');
 var User = mongoose.model('User');
 var auth = require('../auth');
+var userService = require('./service/usersService')
 
-router.get('/user', auth.required, function(req, res, next){
-  User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401); }
+router.get('/user', auth.required, function (req, res, next) {
 
-    return res.json({user: user.toAuthJSON()});
-  }).catch(next);
+  userService.getUser(req)
+    .then((user) => {
+      return res.json(user);
+    })
+    .catch(() => {
+      return res.sendStatus(401);
+    })
+
+  // User.findById(req.payload.id).then(function(user){
+  //   if(!user){ return res.sendStatus(401); }
+
+  //   return res.json({user: user.toAuthJSON()});
+  // }).catch(next);
 });
 
-router.put('/user', auth.required, function(req, res, next){
-  User.findById(req.payload.id).then(function(user){
-    if(!user){ return res.sendStatus(401); }
+router.put('/user', auth.required, function (req, res, next) {
 
-    // only update fields that were actually passed...
-    if(typeof req.body.user.username !== 'undefined'){
-      user.username = req.body.user.username;
-    }
-    if(typeof req.body.user.email !== 'undefined'){
-      user.email = req.body.user.email;
-    }
-    if(typeof req.body.user.bio !== 'undefined'){
-      user.bio = req.body.user.bio;
-    }
-    if(typeof req.body.user.image !== 'undefined'){
-      user.image = req.body.user.image;
-    }
-    if(typeof req.body.user.password !== 'undefined'){
-      user.setPassword(req.body.user.password);
-    }
+  userService.putUser(req)
+    .then((user) => {
+      return res.json(user)
+    })
 
-    return user.save().then(function(){
-      return res.json({user: user.toAuthJSON()});
-    });
-  }).catch(next);
+  // User.findById(req.payload.id).then(function(user){
+  //   if(!user){ return res.sendStatus(401); }
+
+  //   // only update fields that were actually passed...
+  //   if(typeof req.body.user.username !== 'undefined'){
+  //     user.username = req.body.user.username;
+  //   }
+  //   if(typeof req.body.user.email !== 'undefined'){
+  //     user.email = req.body.user.email;
+  //   }
+  //   if(typeof req.body.user.bio !== 'undefined'){
+  //     user.bio = req.body.user.bio;
+  //   }
+  //   if(typeof req.body.user.image !== 'undefined'){
+  //     user.image = req.body.user.image;
+  //   }
+  //   if(typeof req.body.user.password !== 'undefined'){
+  //     user.setPassword(req.body.user.password);
+  //   }
+
+  //   return user.save().then(function(){
+  //     return res.json({user: user.toAuthJSON()});
+  //   });
+  // }).catch(next);
 });
 
-router.post('/users/login', function(req, res, next){
-  if(!req.body.user.email){
-    return res.status(422).json({errors: {email: "can't be blank"}});
+router.post('/users/login', function (req, res, next) {
+  if (!req.body.user.email) {
+    return res.status(422).json({ errors: { email: "can't be blank" } });
   }
 
-  if(!req.body.user.password){
-    return res.status(422).json({errors: {password: "can't be blank"}});
+  if (!req.body.user.password) {
+    return res.status(422).json({ errors: { password: "can't be blank" } });
   }
 
-  passport.authenticate('local', {session: false}, function(err, user, info){
-    if(err){ return next(err); }
+  passport.authenticate('local', { session: false }, function (err, user, info) {
+    if (err) { return next(err); }
 
-    if(user){
+    if (user) {
       user.token = user.generateJWT();
-      return res.json({user: user.toAuthJSON()});
+      return res.json({ user: user.toAuthJSON() });
     } else {
       return res.status(422).json(info);
     }
   })(req, res, next);
 });
 
-router.post('/users', function(req, res, next){
+router.post('/users', function (req, res, next) {
   var user = new User();
 
   user.username = req.body.user.username;
   user.email = req.body.user.email;
   user.setPassword(req.body.user.password);
 
-  user.save().then(function(){
-    return res.json({user: user.toAuthJSON()});
+  user.save().then(function () {
+    return res.json({ user: user.toAuthJSON() });
   }).catch(next);
 });
 
